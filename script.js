703f24556e9cacbe014e6a0d6581411d1e824610
Eurus <eurus0601@gmail.com>
2022-08-11 12:01:07 +0800

    navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector(".header__hamburger");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("header__hamburger--active");
+  console.log();
+});
