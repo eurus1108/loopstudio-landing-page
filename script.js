6de86b9e02c47ce19ca5e035fccda01c13731730
Eurus <eurus0601@gmail.com>
2022-08-13 20:23:57 +0800

    navbar mobile styling && minor fixes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const hamburgerMenu = document.querySelector(".header__hamburger");
+const modalMenu = document.querySelector(".modal");
+const body = document.querySelector("body");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("header__hamburger--active");
-  console.log();
+
+  if (hamburgerMenu.classList.contains("header__hamburger--active")) {
+    body.classList.add("no-scroll");
+    modalMenu.classList.add("modal--active");
+  } else {
+    body.classList.remove("no-scroll");
+    modalMenu.classList.remove("modal--active");
+  }
 });
